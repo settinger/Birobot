Fixed the animation errors and frame calculation

py/biro_sim.py
@@ -3,12 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
+import matplotlib
+matplotlib.use('TkAgg')
 
 import birobot
 from biro_constants_bad import *
 
 # Get list of actions to simulate
-waypts = [(0, 0, -60), (0, 0, -50), (10, 0, -40), (10, 10, -40), (-10, 10, -40), (-10, -10, -40), (10, -10, -40), (10, 0, -40), (0, 0, -40)]
+waypts = [(0, 0, -60), (0, 0, -70), (10, 0, -80), (10, 10, -80), (-10, 10, -40), (-10, -10, -40), (10, -10, -40), (10, 0, -40), (0, 0, -40)]
+##waypts = [(0, 0, 0), (0, 0, -10)]
 
 # Initialize frame
 fig = plt.figure()
@@ -37,38 +40,31 @@ for waypt in waypts:
         
         (qA, qB, qC) = birobot.xyz2servo(*dest)
         # TEMPHACK:
-        qA += 180; qB += 180; qC += 180;
+        # qA += 180; qB += 180; qC += 180;
         # Rotation matrix from frame N to frames of upper links:
         R_A_N = birobot.rotationMatrix([1,0,0], qA*np.pi/180)
-        R_B_N = birobot.rotationMatrix([0,0,1], 2*np.pi/3) * birobot.rotationMatrix([1,0,0], qB*np.pi/180)
-        R_C_N = birobot.rotationMatrix([0,0,1], 4*np.pi/3) * birobot.rotationMatrix([1,0,0], qC*np.pi/180)
+        R_B_N = birobot.rotationMatrix([1,0,0], qB*np.pi/180) * birobot.rotationMatrix([0,0,1], 2*np.pi/3)
+        R_C_N = birobot.rotationMatrix([1,0,0], qC*np.pi/180) * birobot.rotationMatrix([0,0,1], 4*np.pi/3)
+##        R_B_N = birobot.rotationMatrix([0,0,1], 2*np.pi/3) * birobot.rotationMatrix([1,0,0], qB*np.pi/180)
+##        R_C_N = birobot.rotationMatrix([0,0,1], 4*np.pi/3) * birobot.rotationMatrix([1,0,0], qC*np.pi/180)
 
         # Define frame indexes (relative to inertial frame)
         n_x = np.matrix([1,0,0]).T
         n_y = np.matrix([0,1,0]).T
         n_z = np.matrix([0,0,1]).T
-        a_x = R_A_N*n_x
-        a_y = R_A_N*n_y
-        a_z = R_A_N*n_z
-        b_x = R_B_N*n_x
-        b_y = R_B_N*n_y
-        b_z = R_B_N*n_z
-        c_x = R_C_N*n_x
-        c_y = R_C_N*n_y
-        c_z = R_C_N*n_z
-##        a_x = R_A_N.I*np.matrix([1,0,0]).T
-##        a_y = R_A_N.I*np.matrix([0,1,0]).T
-##        a_z = R_A_N.I*np.matrix([0,0,1]).T
-##        b_x = R_B_N.I*np.matrix([1,0,0]).T
-##        b_y = R_B_N.I*np.matrix([0,1,0]).T
-##        b_z = R_B_N.I*np.matrix([0,0,1]).T
-##        c_x = R_C_N.I*np.matrix([1,0,0]).T
-##        c_y = R_C_N.I*np.matrix([0,1,0]).T
-##        c_z = R_C_N.I*np.matrix([0,0,1]).T
-
+        a_x = R_A_N.I*n_x
+        a_y = R_A_N.I*n_y
+        a_z = R_A_N.I*n_z
+        b_x = R_B_N.I*n_x
+        b_y = R_B_N.I*n_y
+        b_z = R_B_N.I*n_z
+        c_x = R_C_N.I*n_x
+        c_y = R_C_N.I*n_y
+        c_z = R_C_N.I*n_z
+        
         A0 = -sB/2*n_y
-        B0 = birobot.rotationMatrix([0,0,1], 2*np.pi/3)*A0
-        C0 = birobot.rotationMatrix([0,0,1], 2*np.pi/3)*B0
+        B0 = birobot.rotationMatrix([0,0,1], -2*np.pi/3)*A0
+        C0 = birobot.rotationMatrix([0,0,1], -2*np.pi/3)*B0
         A1 = A0 - L*a_y
         B1 = B0 - L*b_y
         C1 = C0 - L*c_y
@@ -82,7 +78,7 @@ for waypt in waypts:
         xlist = [sB/2*np.cos(q) for q in np.linspace(0, 2*np.pi, 7)]
         ylist = [sB/2*np.sin(q) for q in np.linspace(0, 2*np.pi, 7)]
         zlist = [0 for q in np.linspace(0, 2*np.pi, 7)]
-        ax.plot(xlist, ylist, zlist, 'b-')
+        ax.plot(xlist, ylist, zlist, 'g-')
         
         # Draw the triangle that is the end effector
         xlist = [xd + sP/2*np.cos(q) for q in np.linspace(0, 2*np.pi, 7)]
@@ -99,10 +95,20 @@ for waypt in waypts:
         ax.plot(Clink[0], Clink[1], Clink[2], 'r:')
         
         # Draw the parallelograms
+        Alink = [(PA[n].sum(), A1[n].sum()) for n in [0, 1, 2]]
+        Blink = [(PB[n].sum(), B1[n].sum()) for n in [0, 1, 2]]
+        Clink = [(PC[n].sum(), C1[n].sum()) for n in [0, 1, 2]]
+        ax.plot(Alink[0], Alink[1], Alink[2], 'b:')
+        ax.plot(Blink[0], Blink[1], Blink[2], 'b:')
+        ax.plot(Clink[0], Clink[1], Clink[2], 'b:')
 
         ax.set_xlim3d((-50,50))
         ax.set_ylim3d((-50,50))
         ax.set_zlim3d((-100,0))
+
+        vec = PA - A1
+        vec = vec.T*vec
+        print(str(qA) + ', ' + str(qB) + ', ' + str(qC))
         #plt.show()
         plt.pause(0.1)
     oldpt = waypt

py/birobot.py
@@ -1,34 +1,34 @@
 import numpy as np
 from numpy import sqrt
-import scipy
+from scipy.optimize import fsolve
 
 from biro_constants_bad import *
 
 
 # Rotation matrix from a Scipy mailing list, March 2009
 def rotationMatrix(direction, angle):
-     """
-     Create a rotation matrix corresponding to the rotation around a general
-     axis by a specified angle.
-
-     R = dd^T + cos(a) (I - dd^T) + sin(a) skew(d)
-
-     Parameters:
-
-         angle : float a
-         direction : array d
-     """
-     d = np.array(direction, dtype=np.float64)
-     d /= np.linalg.norm(d)
-
-     eye = np.eye(3, dtype=np.float64)
-     ddt = np.outer(d, d)
-     skew = np.matrix([[    0,  d[2],  -d[1]],
-                      [-d[2],     0,  d[0]],
-                      [d[1], -d[0],    0]], dtype=np.float64)
-
-     mtx = ddt + np.cos(angle) * (eye - ddt) + np.sin(angle) * skew
-     return mtx
+    """
+    Create a rotation matrix corresponding to the rotation around a general
+    axis by a specified angle.
+    
+    R = dd^T + cos(a) (I - dd^T) + sin(a) skew(d)
+    
+    Parameters:
+    
+    angle : float a
+    direction : array d
+    """
+    d = np.array(direction, dtype=np.float64)
+    d /= np.linalg.norm(d)
+    
+    eye = np.eye(3, dtype=np.float64)
+    ddt = np.outer(d, d)
+    skew = np.matrix([[    0,  d[2],  -d[1]],
+                  [-d[2],     0,   d[0]],
+                   [d[1], -d[0],     0]], dtype=np.float64)
+    
+    mtx = ddt + np.cos(angle) * (eye - ddt) + np.sin(angle) * skew
+    return mtx
 
 def quadraticSolve(E, F, G):
     t1 = (-F + sqrt(E*E + F*F - G*G))/(G-E)
@@ -62,23 +62,33 @@ def xyz2servo(x, y, z):
 def servo2xyz(qA, qB, qC):
     # Rotation matrix from frame N to frames of upper links:
     R_A_N = rotationMatrix([1,0,0], qA*np.pi/180)
-    R_B_N = rotationMatrix([0,0,1], 2*np.pi/3) * rotationMatrix([1,0,0], qB*np.pi/180)
-    R_C_N = rotationMatrix([0,0,1], 4*np.pi/3) * rotationMatrix([1,0,0], qC*np.pi/180)
+    R_B_N = rotationMatrix([1,0,0], qB*np.pi/180) * rotationMatrix([0,0,1], 2*np.pi/3)
+    R_C_N = rotationMatrix([1,0,0], qC*np.pi/180) * rotationMatrix([0,0,1], 4*np.pi/3)
 
     # Define frame indexes
-    a_x = R_A_N.I*np.matrix([1,0,0]).T
-    a_y = R_A_N.I*np.matrix([0,1,0]).T
-    a_z = R_A_N.I*np.matrix([0,0,1]).T
-    b_x = R_B_N.I*np.matrix([1,0,0]).T
-    b_y = R_B_N.I*np.matrix([0,1,0]).T
-    b_z = R_B_N.I*np.matrix([0,0,1]).T
-    c_x = R_C_N.I*np.matrix([1,0,0]).T
-    c_y = R_C_N.I*np.matrix([0,1,0]).T
-    c_z = R_C_N.I*np.matrix([0,0,1]).T
+    n_x = np.matrix([1,0,0]).T
+    n_y = np.matrix([0,1,0]).T
+    n_z = np.matrix([0,0,1]).T
+    a_x = R_A_N.I*n_x
+    a_y = R_A_N.I*n_y
+    a_z = R_A_N.I*n_z
+    b_x = R_B_N.I*n_x
+    b_y = R_B_N.I*n_y
+    b_z = R_B_N.I*n_z
+    c_x = R_C_N.I*n_x
+    c_y = R_C_N.I*n_y
+    c_z = R_C_N.I*n_z
+
+    A0 = -sB/2*n_y
+    B0 = birobot.rotationMatrix([0,0,1], 2*np.pi/3)*A0
+    C0 = birobot.rotationMatrix([0,0,1], 2*np.pi/3)*B0
+    A1 = A0 - L*a_y
+    B1 = B0 - L*b_y
+    C1 = C0 - L*c_y
 
     # Position vectors
     #r_Pa_N0 = 
-    
+
     return 0
 
 def equations(p):